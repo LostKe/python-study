13e6c4151b10b0135fbf5b88a5279b8868fbd8ee
lost <shu_qingzhang@foxmail.com>
2017-01-02 10:35:55 +0800

    up

--- test3.py
+++ test3.py
@@ -2,6 +2,7 @@
 
 array = ["a", "b", "c"]
 
+# join 函数 将Iterable 对象连接起来
 message = ",".join(array)
 
 print(message)
@@ -9,3 +10,10 @@ print(message)
 mapping = {'aaa': 11, 'cc': 12}
 mapping["ddddd"] = 999
 print(mapping)
+
+from  collections import Iterable
+
+print(isinstance("adsd", Iterable))
+
+
+print(":".join("abcdefg"))
