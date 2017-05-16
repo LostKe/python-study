acc6b54c4639e0063e5f774a5b9afbc63805ce05
lost <shu_qingzhang@foxmail.com>
2017-05-16 22:59:52 +0800

    up

--- http/wuzhi.py
+++ http/wuzhi.py
@@ -3,16 +3,11 @@
 爬取吾志用户的头像
 url: https://wuzhi.me/u/xxx  需要爬取数据的地址
 '''
-import urllib
-from urllib import request
+import requests
+import requests.packages.urllib3.util.ssl_
+requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
 
-URL_HEAD = "http://www.douban.com/"
+URL_HEAD = "https://wuzhi.me/u/148189"
 USER_AGENT = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
-req = request.Request(URL_HEAD)
-req.add_header("User-Agent", USER_AGENT)
-
-with request.urlopen(req) as f:
-    print("status", f.status, f.reason)
-    for key, value in f.getheaders():
-        print("%s:%s" % (key, value))
-    print("Data:", f.read().decode("utf-8"))
+response = requests.get(URL_HEAD)
+print(response.text)
