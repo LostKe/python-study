43e3bdafc55b1e719a73b15d3c09b7cde8bf6ba1
zsq <shu_qingzhang@foxmail.com>
2017-05-18 16:16:06 +0800

    add temp file

--- http/wuzhi.py
+++ http/wuzhi.py
@@ -14,29 +14,42 @@ requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
 from bs4 import BeautifulSoup
 
 IMG_STORE_PATH="/wuzhi/img/"
-URL_HEAD = "https://wuzhi.me/u/43013"
+URL_HEAD = "https://wuzhi.me/u/"
 USER_AGENT = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
-response = requests.get(URL_HEAD)
-html = response.text.encode("utf-8")
 
-soup = BeautifulSoup(html, "html.parser")
-head = soup.find("head")
-title = soup.find("title")
-# print(head)
-print("作者:%s" % title.get_text())
+def getRes(url_head,user_id):
+    url=url_head+user_id
+    res=requests.get(url)
+    status_code = res.status_code
+    if status_code==requests.codes.ok:
+        return res
+    else:
+        print("用户：%s无法访问" % user_id)
+        return -1
 
-siderbar_left_div = soup.find("div", {"class", "siderbar_left"})
-img_url = siderbar_left_div.find("img").attrs["src"]
-print("头像地址:%s" % img_url)
+def spider(url_head,user_id):
+    res=getRes(url_head,user_id)
+    if -1!=res:
+        html = res.text.encode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        head = soup.find("head")
+        title = soup.find("title")
+        print("作者:%s" % title.get_text())
+        siderbar_left_div = soup.find("div", {"class", "siderbar_left"})
+        img_url = siderbar_left_div.find("img").attrs["src"]
+        print("头像地址:%s" % img_url)
+        array_temp = img_url.split("/")
+        user_id = array_temp[len(array_temp) - 1].split(".")[0]
+
+        print("用户ID:%s" % user_id)
+
+        flower_count = soup.find(text=re.compile("×")).replace("×", "")
+        flower_count = flower_count.strip()
+        print("小花数量:%s" % flower_count)
+
+        downloadImg.storeImg(img_url, IMG_STORE_PATH, user_id)
 
 
-array_temp=img_url.split("/")
-user_id=array_temp[len(array_temp)-1].split(".")[0]
+if __name__ == '__main__':
+    spider(URL_HEAD,43013)
 
-print("用户ID:%s" % user_id)
-
-flower_count=soup.find(text=re.compile("×")).replace("×","")
-flower_count=flower_count.strip()
-print("小花数量:%s" % flower_count)
-
-downloadImg.storeImg(img_url,IMG_STORE_PATH,user_id)
