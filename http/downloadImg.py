62863b5b1486693d0b36ebe25a018c218a955b46
zsq <shu_qingzhang@foxmail.com>
2017-05-18 14:42:08 +0800

    add temp file

---
+++ http/downloadImg.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# coding=utf-8
+'''
+下载头像
+'''
+
+import os
+from os import path
+from urllib import request
+def storeImg(imgUrl,targetPath,userId):
+    isExist=os.path.exists(targetPath)
+    if isExist !=True :
+        #文件夹不存在创建文件夹
+        os.mkdir(targetPath)
+    #获取文件保存本地
+    targetImg = targetPath + "/" + userId+".jpg"
+    with request.urlopen(imgUrl) as _data:
+        with open(targetPath,'wb') as outfile:
+            outfile.write(_data.read())
+    print("用户:%s头像下载完毕！" % userId)
+
--- http/wuzhi.py
+++ http/wuzhi.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # coding=utf-8
 '''
 爬取吾志用户的头像
@@ -6,10 +7,15 @@ url: https://wuzhi.me/u/xxx  需要爬取数据的地址
 import requests
 import re
 import requests.packages.urllib3.util.ssl_
+import sys
+import http.downloadImg
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
 from bs4 import BeautifulSoup
 
+IMG_STORE_PATH="/wuzhi/img/"
 URL_HEAD = "https://wuzhi.me/u/43013"
 USER_AGENT = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
 response = requests.get(URL_HEAD)
@@ -25,9 +31,12 @@ siderbar_left_div = soup.find("div", {"class", "siderbar_left"})
 img_url = siderbar_left_div.find("img").attrs["src"]
 print("头像地址:%s" % img_url)
 
+
 array_temp=img_url.split("/")
 user_id=array_temp[len(array_temp)-1].split(".")[0]
 print("用户ID:%s" % user_id)
 
 flower_count=soup.find(text=re.compile("×")).replace("×","")
 print("小花数量:%s" % flower_count)
+
+http.downloadImg.storeImg(img_url,IMG_STORE_PATH,user_id)
