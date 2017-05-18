3bcc5b5b4d50b6faa8fc48a737cf5b23adb1f020
zsq <shu_qingzhang@foxmail.com>
2017-05-18 20:54:00 +0800

    add temp file

--- http/wuzhi.py
+++ http/wuzhi.py
@@ -4,6 +4,7 @@
 爬取吾志用户的头像
 url: https://wuzhi.me/u/xxx  需要爬取数据的地址
 '''
+import asyncio
 import requests
 import re
 import requests.packages.urllib3.util.ssl_
@@ -16,6 +17,7 @@ from bs4 import BeautifulSoup
 IMG_STORE_PATH="/wuzhi/img/"
 URL_HEAD = "https://wuzhi.me/u/"
 USER_AGENT = "Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)"
+MAX_USER_ID=272690
 
 def getRes(url_head,user_id):
     url=url_head+str(user_id)
@@ -27,6 +29,7 @@ def getRes(url_head,user_id):
         print("用户：%s无法访问" % user_id)
         return -1
 
+@asyncio.coroutine
 def spider(url_head,user_id):
     res=getRes(url_head,user_id)
     if -1!=res:
@@ -36,7 +39,13 @@ def spider(url_head,user_id):
         title = soup.find("title")
         print("作者:%s" % title.get_text())
         siderbar_left_div = soup.find("div", {"class", "siderbar_left"})
-        img_url = siderbar_left_div.find("img").attrs["src"]
+        #检查图片是否存在
+        img=siderbar_left_div.has_attr("src")
+        if img!=True:
+            print("用户：%s头像不存在" % user_id)
+            img_url=None
+        else:
+            img_url = siderbar_left_div.find("img").attrs["src"]
         print("头像地址:%s" % img_url)
         array_temp = img_url.split("/")
         user_id = array_temp[len(array_temp) - 1].split(".")[0]
@@ -46,10 +55,18 @@ def spider(url_head,user_id):
         flower_count = soup.find(text=re.compile("×")).replace("×", "")
         flower_count = flower_count.strip()
         print("小花数量:%s" % flower_count)
+        if img_url!=None:
+            downloadImg.storeImg(img_url, IMG_STORE_PATH, user_id)
 
-        downloadImg.storeImg(img_url, IMG_STORE_PATH, user_id)
+@asyncio.coroutine
+def index_spider():
+    for index in range(1,MAX_USER_ID):
+        r=yield from asyncio.spider(URL_HEAD,index)
+
 
 
 if __name__ == '__main__':
-    spider(URL_HEAD,43013)
+    loop=asyncio.get_event_loop()
+    loop.run_until_complete(index_spider())
+    loop.close()
 
