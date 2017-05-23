42e238170f06021f7a050625b331d8b0bbad1250
lost <shu_qingzhang@foxmail.com>
2017-05-23 22:42:32 +0800

    up

--- http/wuzhi.py
+++ http/wuzhi.py
@@ -75,8 +75,8 @@ def spider(url_head, user_id):
             #查找小花的数量
 
             flower_img=soup.find_all(src="/img/flower-16.png")[0]
-            flower_count=flower_img.parent.contents[1]
-            print(flower_count)
+            flower_count=flower_img.parent.contents[1].replace("×", "").strip()
+            print("小花数量%s" % flower_count)
             # 没有头像的也得存入db中统计
             downloadImg.storeImg(user_name,signature,img_url, IMG_STORE_PATH, int(user_id),int(flower_count))
     except Exception as e:
